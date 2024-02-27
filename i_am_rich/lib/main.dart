fcdc8bc8c5921a3f6e94241c263b11f6c2b120ce
Rajkiran Bande <bande.rajkiran@gmail.com>
2024-02-27 20:04:15 +0530

    I am rich

---
+++ i_am_rich/lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Text(
+            "I am Rich!",
+            style: TextStyle(color: Colors.white)
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
